0591903366fd6974d296e2414871c8bcc6fc2c23
GaiSensei <dawnis.chow@cornell.edu>
2018-06-15 00:43:53 -0700

    late night update

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -3,10 +3,11 @@ from flaskapp import app
 from tkinter.filedialog import askopenfilename
 from tkinter import Tk
 import os
-from src.fashion_tools import image_to_feature
-
+from src.fashion_tools import image_to_feature, DeepFashion, similarity_function, rgb_image_bounding_box
+import numpy as np
 from keras.models import load_model
 import time
+import cv2
 
 #global encoder
 #encoder = load_model("models/encoder_model_current.h5")
@@ -14,25 +15,40 @@ import time
 #decoder = load_model("models/decoder_model_current.h5")
 #decoder.load_weights("models/decoder_model_weights_current.h5")
 
-fullpath_to_data = "/home/dawnis/Data/SmartMirror/DeepFashion_Data"
+deepDict = DeepFashion("Top")
+global deepKeys
+deepKeys = [keyname for keyname in deepDict.keys()]
+
+
+global allFeatures
+allFeatures = np.load("models/current_feature_vector.npy")
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-
-    imgfile = {"filepath" : os.path.join(app.instance_path, "/static/images/go-away-oscar-the-grouch-t-shirt.master.png")}
+    imgfile = {"filepath" :  "static/images/go-away-oscar-the-grouch-t-shirt.master.png"}
     return render_template("index.html", title="Home", imgfile=imgfile)
 
-@app.route('/mirror/<imgfile.filepath>')
-def smart_mirror(imgfile):
+@app.route('/mirror/<path:imgpath>')
+def smart_mirror(imgpath):
     #global encoder
+    fullpath_to_data = "/home/dawnis/Data/SmartMirror/DeepFashion_Data"
     encoder = load_model("models/encoder_model_current.h5")
     encoder.load_weights("models/encoder_model_weights_current.h5")
-    time.sleep(2)
+    time.sleep(3)
     #aimg = "flaskapp/static/images/bundle1/key.png"
-    aimg = imgfile.relative_path
+    aimg = os.path.join("flaskapp", imgpath)
     feature_vector_main = image_to_feature(aimg, [], encoder)
+    scores = [similarity_function(feature_vector_main, partner) for partner in allFeatures]
+    closest = np.argsort(np.array(scores))
+
+    for idx, x in enumerate(closest[:6]):
+        keyname = deepKeys[x]
+        image = rgb_image_bounding_box("/".join([fullpath_to_data, keyname]), deepDict[keyname])
+        cv2.imwrite('flaskapp/static/images/match{:03d}.png'.format(idx+1), image)
+
+    #deepKeys[closest]
     #print(feature_vector_main)
     return render_template("mirror_display.html", title="Smart Mirror App")
 
